6c8f57260f5918894c399fb8d176de1f9fc4c4a9
Armaan <armaank140@gmail.com>
2023-02-24 23:25:53 +0530

    Add top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -22,16 +22,21 @@ export class News extends Component {
   // Code Refactoring
   //Create a Function to fatch data through API 
   async updateNews(){
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c5773707394445a4ae866c469da32e99&page=${this.props.page}&pagesize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
-    console.log(parsedData);
+    this.props.setProgress(70);
+   // console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
+
   }
 
   async componentDidMount() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -17,12 +17,15 @@ export class NewsItem extends Component {
             alt="/"
           />
           <div className="card-body">
-          <span class="position-absolute top-0  translate-middle badge rounded-pill bg-primary" style={{left:'90%' , zIndex:'1'}}>
+           
+              <span
+                class="position-absolute top-0  translate-middle badge rounded-pill bg-primary"
+                style={{ left: "90%", zIndex: "1" }}
+              >
                 {source}
               </span>
-            <h5 className="card-title">
-              {title}
-            </h5>
+            
+            <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
 
             <p className="card-text">
